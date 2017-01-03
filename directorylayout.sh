bd195c2408cc2a4e8c45c2111fc52011eb46ce18
Sjacob <shibujacob@hotmail.co.uk>
2017-01-03 19:01:00 +0000

    bash script
    Bash script for the directory layout

---
+++ directorylayout.sh
@@ -0,0 +1,11 @@
+mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,development/{host_vars/{host_dev1,host-dev2},group_vars/{group1,group2}}}
+
+
+mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,staging/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
+
+mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,production/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
+
+
+touch inventories/{site.yml,webservers.yml,appservers.yml,dbservers.yml}
+
+touch inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta}/main.yml
