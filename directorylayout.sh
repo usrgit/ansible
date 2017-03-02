2fb9dbdf0cab6975cd2656bb988e400595c86538
Sjacob <shibujacob@hotmail.co.uk>
2017-03-02 12:07:34 +0000

    Added ansible directory as parent directory

--- directorylayout.sh
+++ directorylayout.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,development/{host_vars/{host_dev1,host-dev2},group_vars/{group1,group2}}}
+mkdir -p ansible/inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,development/{host_vars/{host_dev1,host-dev2},group_vars/{group1,group2}}}
 
 
-mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,staging/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
+mkdir -p ansible/inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,staging/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
 
-mkdir -p inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,production/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
+mkdir -p ansible/inventories/{library,roles/{common/{tasks,handlers,templates,files,vars,defaults,meta},webtier,servicetier,database,monitoring},filter_plugins/,production/{host_vars/{host_stage1,host-stage2},group_vars/{group1,group2}}}
 
 
-touch inventories/{site.yml,webservers.yml,appservers.yml,dbservers.yml}
+touch ansible/inventories/{site.yml,webservers.yml,appservers.yml,dbservers.yml}
 
-touch inventories/roles/common/{tasks,handlers,vars,defaults,meta}/main.yml
+touch ansible/inventories/roles/common/{tasks,handlers,vars,defaults,meta}/main.yml
